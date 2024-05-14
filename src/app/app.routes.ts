b919faf4587c431308dcad59d68e5ee90fd0ddcd
andrewarosario <andrew.arosario@gmail.com>
2024-05-13 21:05:55 -0300

    move to nx

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./home/home.component').then((c) => c.HomeComponent),
+  },
+  {
+    path: 'users',
+    loadComponent: () =>
+      import('./users/users.component').then((c) => c.UsersComponent),
+  },
+];
